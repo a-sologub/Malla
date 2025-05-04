c3d7e38a54d9ed2b70427a63d5528887e35528b0
Andrey Sologub <sologub.a@mailbox.org>
2025-05-04 15:12:23 +0200

    Initial commit

---
+++ Malla/MallaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MallaApp.swift
+//  Malla
+//
+//  Created by Andrey on 04.05.25.
+//
+
+import SwiftUI
+
+@main
+struct MallaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MallaTests/MallaTests.swift
@@ -0,0 +1,17 @@
+//
+//  MallaTests.swift
+//  MallaTests
+//
+//  Created by Andrey on 04.05.25.
+//
+
+import Testing
+@testable import Malla
+
+struct MallaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
